26a15c53f4853da0369fc74bc54241a9fac34049
guywithhat99 <jami6753@colorado.edu>
2026-02-17 23:48:37 -0700

    Initial Commit

---
+++ lotSpotApp/lotSpotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  lotSpotAppApp.swift
+//  lotSpotApp
+//
+//  Created by Jack on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct lotSpotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
